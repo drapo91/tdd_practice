89e6953d54183b946203db63f387a79881275139
drapo <drapo91@gmail.com>
2018-12-07 21:57:45 +0100

    [Ship Kata] Req. 01 implemented

--- src/main/java/com/tddjava/shipkata/Ship.java
+++ src/main/java/com/tddjava/shipkata/Ship.java
@@ -1,5 +1,13 @@
 package com.tddjava.shipkata;
 
 public class Ship {
+    private Location location;
 
+    public Ship(Location location){
+        this.location=location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
--- src/test/java/com/tddjava/shipkata/ShipSpec.java
+++ src/test/java/com/tddjava/shipkata/ShipSpec.java
@@ -2,7 +2,20 @@ package com.tddjava.shipkata;
 
 import org.testng.annotations.*;
 
+import static org.testng.Assert.assertEquals;
+
 @Test
 public class ShipSpec {
+    private Ship ship;
+    private Location location;
 
+    @BeforeMethod
+    public void beforeTest(){
+        location = new Location(new Point(21,13), Direction.EAST);
+        ship = new Ship(location);
+    }
+
+    public void whenInstantiatedThenLocationIsSet(){
+        assertEquals(ship.getLocation(), location);
+    }
 }
